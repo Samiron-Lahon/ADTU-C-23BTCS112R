57da587a642bf3092a2cda8df50a030690776b90
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-03 21:32:49 +0000

    problem-1

--- x/any.c
+++ x/any.c
@@ -4,13 +4,3 @@ int main ()
  printf("Welcome to Assam down town University");
  return 0;
 }
-
-#include<stdio.h>
-int main ()
-{
- int num;
- printf("Enter an Integer:");
- scanf("%d", &num);
- printf("You entered Integer is:");
- return 0;
-}
