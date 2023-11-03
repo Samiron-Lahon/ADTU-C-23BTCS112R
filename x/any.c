152c1c90634ced6fd8c21be2bc01c81489bce736
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-03 21:30:50 +0000

    add

--- x/any.c
+++ x/any.c
@@ -4,3 +4,13 @@ int main ()
  printf("Welcome to Assam down town University");
  return 0;
 }
+
+#include<stdio.h>
+int main ()
+{
+ int num;
+ printf("Enter an Integer:");
+ scanf("%d", &num);
+ printf("You entered Integer is:");
+ return 0;
+}
