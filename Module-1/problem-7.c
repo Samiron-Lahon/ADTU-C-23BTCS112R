86f9901a6d920cf0b68648fe2ccf89510a8ddfec
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:04:25 +0000

    problem-7

---
+++ Module-1/problem-7.c
@@ -0,0 +1,11 @@
+  #include<stdio.h>
+  #include<math.h>
+  int main ()
+  {
+    int a, b, add;
+    printf("Enter the numbers:");
+    scanf("%d %d", &a, &b);
+    add=a+b;
+    printf("The sum of entered numbers is: %d", add);
+    return 0;
+  }
