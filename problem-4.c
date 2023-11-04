13f2f13e86529c0501520bf57c96522706ff7e3e
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:32:11 +0000

    updated readme

--- problem-4.c
+++
@@ -1,9 +0,0 @@
- #include<stdio.h>
-      int main ()
-      {
-        float pointNum;
-        printf("Enter a point number:");
-        scanf("%f", &pointNum);
-        printf("Your entered point number is:");
-        return 0;
-      }
