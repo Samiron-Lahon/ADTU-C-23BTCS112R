589f009765cce86fbdae826335dacbaefa33d5b0
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:22:27 +0000

    problem-17

---
+++ Module-1/problem-17.c
@@ -0,0 +1,12 @@
+  #include<stdio.h>
+  int main ()
+  {
+    int num1, num2, tem;
+    printf("Enter two numbers:");
+    scanf("%d %d", &num1, &num2);
+    tem=num1
+    num1=num2
+    num2=tem
+    printf("Value after swapping: num1= %d and num2= %d\n", num1, num2);
+  return 0;
+  }
