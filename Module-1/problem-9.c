8bcff2ba7405bf2ceafafa4276f8f16489b19102
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:07:19 +0000

    problem-9

---
+++ Module-1/problem-9.c
@@ -0,0 +1,9 @@
+   #include<stdio.h>
+   int main ()
+   {
+    char a;
+    printf("Enter the character:");
+    scanf("%c", &a);
+    printf("The ASCII value of entered %c is %d", a, a);
+    return 0;
+   }
