df51092e716e7fd6d73fddf543b9558f7ada705b
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 10:45:31 +0000

    problem-4

---
+++ problem-4.c
@@ -0,0 +1,9 @@
+ #include<stdio.h>
+      int main ()
+      {
+        float pointNum;
+        printf("Enter a point number:");
+        scanf("%f", &pointNum);
+        printf("Your entered point number is:");
+        return 0;
+      }
