5226c6c7ca23dbbab2ea377c16ccc67131a34639
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:20:13 +0000

    problem-15

---
+++ Module-1/problem-15.c
@@ -0,0 +1,9 @@
+  #include<stdio.h>
+  int main ()
+  {
+    printf("Size of int: %d\n bytes", sizeof(int));
+    printf("Size of float: %d\n  bytes", sizeof(float));
+    printf("Size of double: %d\n bytes", sizeof(double));
+    prinntf("Size of char: %d\n bytes", sizeof(char));
+    return 0;
+  }
