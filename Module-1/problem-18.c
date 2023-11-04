0f134805c490510e9bc16a779aa6a6c48f0bc2c0
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:26:16 +0000

    problem-18

---
+++ Module-1/problem-18.c
@@ -0,0 +1,11 @@
+   #include<stdio.h>
+   #include<math.h>
+   int main ()
+   {
+    double num, sq;
+    printf("Enter a number:");
+    scanf("%lf", &num);
+    sq=num*num;
+    printf("Square of the number is: %f", sq);
+    return 0;
+   }
