a05f3d32a9cf012eb28ec8541d589bb1211a4ef5
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:09:47 +0000

    problem-11

---
+++ Module-1/problem-11.c
@@ -0,0 +1,11 @@
+   #include<stdio.h>
+   #include<math.h>
+   int main ()
+   {
+    float lenght, width, perimeter;
+    printf("Enter lenght and width of the rectangle:");
+    scanf("%f %f", &lenght, &width);
+    perimeter=2 (length + width);
+    printf("The value of perimeter of the rectangle is: %f", perimeter);
+    return 0;
+   }
