b0324702826bfd059db532bac383ab0f5246d3f8
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:17:11 +0000

    problem-13

---
+++ Module-1/problem-13.c
@@ -0,0 +1,11 @@
+   #include<stdio.h>
+   #include<math.h>
+   int main ()
+   {
+    float sub1, sub2, sub3, sub4, sub5, avg;
+    printf("Enter the numbers of the subjects:");
+    scanf("%f %f %f %f %f", &sub1, &sub2, &sub3, &sub4, &sub5);
+    avg=(sub1+sub2+sub3+sub4+sub5)/5:
+    printf("The avg of the subjects is: %f", avg);
+    return 0;
+   }
