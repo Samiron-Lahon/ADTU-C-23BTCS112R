5b11af9b522a39f26dd2b602b767e9134994e5f7
Samiron-Lahon <samironlahon88us@gmail.com>
2023-11-04 11:06:15 +0000

    problem-8

---
+++ Module-1/problem-8.c
@@ -0,0 +1,11 @@
+   #include<stdio.h>
+   #include<math.h>
+   int main ()
+   {
+    float num1, num2, mul;
+    printf("Enter the numbers:");
+    scanf("%f %f", &num1, &num2);
+    mul=num1*num2;
+    printf("The multiple entered numbers is: %f", mul);
+    return 0;
+   }
